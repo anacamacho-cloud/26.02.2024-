f6e0ab5c7223574005c6eee33324e8d6d285d5c4
ana.camacho <129320167+anacamacho-cloud@users.noreply.github.com>
2024-02-27 12:05:02 -0300

    Update trycatch.js

--- trycatch.js
+++ trycatch.js
@@ -4,17 +4,19 @@ var variavel2 = "valor editável";
 
 // se a variavel1 for mudada para "valor não permitido vai aparecer no console a frase abaixo"
 try {
+    //o novo valor da variavel
     variavel1="valor não permitido";
+    //se for mudada aparecerá no console essa frase
     console.log("A variavel2 foi alterada para:", variavel1)
-} catch (e){
-    // se não funcionar vai aparecer um alerta de erro no console dizendo
+} catch (e){ // se não funcionar vai aparecer um alerta de erro no console dizendo
     console.error("Ops! ocorreu um erro",e);
 }
-
-//mesma coisa só muda para variavel2 ao invez da variavel1
-try {
+//se a variavel1 for mudada para a frase abaixo no console aparecerá a frase tal
+try {   
+  //o novo valor da variavel
     variavel2="valor permitido";
+    //se for mudada aparecerá no console essa frase
     console.log("A variavel2 foi alterada para:", variavel2)
-} catch (e){
+} catch (e){// se não funcionar vai aparecer um alerta de erro no console dizendo
     console.error("Ops! ocorreu um erro",e);
 }
